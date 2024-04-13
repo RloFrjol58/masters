00540a0df03735bbd16a84c3771e5e08216d7b1d
ruloq <ruloq@Bean_Machine58>
2024-04-12 23:14:30 -0600

    no

--- src/datos/ControlDatosSE.java
+++ src/datos/ControlDatosSE.java
@@ -1,6 +1,45 @@
 
 package datos;
 
+import Conexion.conexion;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import javax.swing.JOptionPane;
+
 public class ControlDatosSE {
+    
+    private conexion conectar;
+    private ModeloDatosSE modeloSE;
+    private Connection con;
+    
+    public ControlDatosSE(){
+   conectar = new conexion();
+   modeloSE = new ModeloDatosSE();
+}
+    public void insertar(String servicio, String precio){
+  
+        PreparedStatement ps;
+        String sql;
+        //modelo.setID(ID);
+        modeloSE.setServicio(servicio);
+        modeloSE.setPrecio(precio);
+        
+        try {
+            	
+            con = conectar.getConnection();
+            sql = "insert into servicio(servicio, precio) values(?,?)";
+            ps = con.prepareStatement(sql);
+            
+            ps.setString(1, modeloSE.getServicio());
+            ps.setString(2, modeloSE.getPrecio());
+        
+            ps.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Se han insertado los datos");
+        
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error de conexión:" + e.getMessage());
+        }
+      }
     
 }
